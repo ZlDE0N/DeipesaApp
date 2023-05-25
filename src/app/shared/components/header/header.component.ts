42c5e1464b5e521063384f0146fc872cd979084e
Cris <cristopherb72@outlook.com>
2023-05-24 23:04:05 -0600

    Correcion ruta imagen

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+public imagenLogo: string="https://firebasestorage.googleapis.com/v0/b/deipesa.appspot.com/o/Recurso%204.png?alt=media&token=c69292ab-2acf-4145-9daf-a326eb43d0e3";
 }
