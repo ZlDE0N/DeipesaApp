a7de586532bc654a68fb02ae78f60d9b5e559ce5
Cris <cristopherb72@outlook.com>
2023-06-05 22:16:04 -0600

    Cambios sobre la marcha

--- src/app/shared/components/controles/portfolio-comp/portfolio-comp.component.ts
+++ src/app/shared/components/controles/portfolio-comp/portfolio-comp.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class PortfolioCompComponent {
 
+  content:{img:string, titulo: string, descripcion:string, btn_text: string}[] = [
+    {
+      img:"https://firebasestorage.googleapis.com/v0/b/deipesa.appspot.com/o/construction%20service%202-compressed.jpg?alt=media&token=aa08756f-0a3a-4616-957f-f52bf99bd159",
+      titulo:'Building Make',
+      descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eligendi moditemporibus alias iste. Accusantium?',
+      btn_text: 'Learn More'
+    },
+    {
+      img:"https://firebasestorage.googleapis.com/v0/b/deipesa.appspot.com/o/construction%20service%202-compressed.jpg?alt=media&token=aa08756f-0a3a-4616-957f-f52bf99bd159",
+      titulo:'Building Make',
+      descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eligendi moditemporibus alias iste. Accusantium?',
+      btn_text:'Learn More',
+    },
+    {
+      img:"https://firebasestorage.googleapis.com/v0/b/deipesa.appspot.com/o/construction%20service%202-compressed.jpg?alt=media&token=aa08756f-0a3a-4616-957f-f52bf99bd159",
+      titulo:'Building Make',
+      descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eligendi moditemporibus alias iste. Accusantium?',
+      btn_text:'Learn More'
+    }
+]
+
 }
