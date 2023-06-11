ce7ce0ed97588d4f9caf2b3f39f094c6f00ad001
Cris <cristopherb72@outlook.com>
2023-06-10 23:02:31 -0600

    Añadimos Page Dashboard

--- src/app/home/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/home/pages/dashboard-page/dashboard-page.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class DashboardPageComponent {
 
+
+  status = false;
+  addToggle() {
+    this.status = !this.status;
+  }
 }
