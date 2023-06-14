48bfe8829f29a9df989fa0b6241372fa13ede63c
Antonioescp <packageinstaller2@gmail.com>
2023-06-13 22:43:37 -0600

    Added proxy and environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiBaseUrl: 'http://localhost:4200/api/',
+  proveedoresEndpoint: 'Proveedores/',
+  materialesEndpoint: 'Materiales/',
+  facturasEndpoint: 'Facturas/',
+};
